f6c0e60b9d0240e43d20858d094ab19659950b0f
Pratham Gupta <prathamgupta0405@gmail.com>
2026-02-05 18:56:17 +0530

    learned printing 1 - N and sum of N numbers

--- Recursion/morerecursion.cpp
+++ Recursion/morerecursion.cpp
@@ -15,17 +15,38 @@ void printonetoN(int N, int count){
   printonetoN(N, count + 1);
 }
 
-void printNtoone(int count){
-  if(count < 1) return;
-  cout << count << endl;
-  printNtoone(count - 1);
+void printNtoone(int current){
+  if(current < 1) return;
+  cout << current << " ";
+  printNtoone(current - 1); //recursive call with next small number
+
+  //backward tracking
+  // printNtoone(current - 1);
+  // cout << current << " ";
+}
+
+//parameterized way
+void sumNP(int sum, int n){
+  if(n < 1){
+    cout << sum << endl;
+    return;
+  }
+  sumNP(sum + n, n - 1);
+}
+
+//functional way
+int sumNF(int n) {
+  if(n == 0) return 0;
+  return n + sumNF(n-1);
 }
 
 int main(){
   string name = "Pratham";
-  int N = 5;
+  int N = 3;
   // printName(name, N, 0);
   // printonetoN(N, 1);
-  printNtoone(N);
+  // printNtoone(N);
+  // sumN(0, N);
+  cout << sumNF(N);
   return 0;
 }
