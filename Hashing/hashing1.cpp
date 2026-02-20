a218b662cee83c6b0c5509d8152caffa7879dd1c
Pratham Gupta <prathamgupta0405@gmail.com>
2026-02-20 15:00:05 +0530

    add: implement countDup function to count occurrences of a number in an array

--- Hashing/hashing1.cpp
+++ Hashing/hashing1.cpp
@@ -1,6 +1,20 @@
 #include<bits/stdc++.h>
 using namespace std;
+
+int countDup(int num, int arr[], int n){
+  int count = 0;
+  for(int i = 0; i < n; i++){
+    if(arr[i] == num){
+      count = count + 1;
+    }
+  }
+  return count;
+}
+
 int main(){
-  
+  int num = 1;
+  int arr[] = {1,2,1,3,2};
+  int n = 5;
+  cout << countDup(num, arr, n);
   return 0;
 }
