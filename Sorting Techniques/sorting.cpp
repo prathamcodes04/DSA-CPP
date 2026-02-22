1517bb0f723da62210725ce2b593568ecd8699ae
Pratham Gupta <prathamgupta0405@gmail.com>
2026-02-23 02:43:36 +0530

    bubble sort

--- Sorting Techniques/sorting.cpp
+++ Sorting Techniques/sorting.cpp
@@ -18,12 +18,32 @@ void selection_sort(int n, int arr[]){
   }
 }
 
+void bubble_sort(int n, int arr[]){
+  for(int i = 0; i < n; i++){
+    bool swapped = false;
+    for(int j = 0; j < n-i-1; j++){
+      if(arr[j] > arr[j+1]){
+        swap(arr[j], arr[j+1]);
+        swapped = true;
+      }
+    }
+    if(!swapped) break;
+    cout << "runs\n";
+  }
+}
+
+void insertion_sort(int n, arr[]){
+  
+}
+
 int main(){
   int n;
   cin >> n;
   int arr[n];
   for(int i = 0; i < n; i++) cin >> arr[i];
-  selection_sort(n , arr);
+  // selection_sort(n , arr);
+  // bubble_sort(n , arr);
+  insertion_sort(n , arr);
   for(int i = 0; i < n; i++) cout << arr[i] << " ";
   return 0;
 }
