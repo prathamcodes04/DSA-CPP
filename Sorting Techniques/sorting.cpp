4105789b8aa08d1484e3d19548ea9d78a1414bdf
Pratham Gupta <prathamgupta0405@gmail.com>
2026-02-23 02:01:02 +0530

    refactor: enhance comments for clarity in selection sort implementation

--- Sorting Techniques/sorting.cpp
+++ Sorting Techniques/sorting.cpp
@@ -1,13 +1,17 @@
 #include<bits/stdc++.h>
 using namespace std;
 void selection_sort(int n, int arr[]){
+  //traverse the array
   for(int i = 0; i <= n-2; i++){
-    int mini = i;
+    int mini = i; //assuming current index holds minimum value
+
+    //find index of the smallest element in the remaining array
     for(int j = i; j <= n-1; j++){
       if(arr[j] < arr[mini]){
-        mini = j;
+        mini = j; //update mini if smaller element is found
       }
     }
+    //swapping
     int temp = arr[mini];
     arr[mini] = arr[i];
     arr[i] = temp;
