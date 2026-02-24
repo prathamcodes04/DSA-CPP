f40344b711c9ab17ddd9d7566d7421f8254e82f7
Pratham Gupta <prathamgupta0405@gmail.com>
2026-02-24 16:19:45 +0530

    merge sort

--- Sorting Techniques/sorting.cpp
+++ Sorting Techniques/sorting.cpp
@@ -44,6 +44,50 @@ void insertion_sort(int n, int arr[]){
   }
 }
 
+void merge(int arr[], int low, int mid, int high){
+  //defining temporary array to store elements
+  vector <int> temp;
+  int left = low;
+  int right = mid + 1;
+
+  while(left <= mid && right <= high){
+    if(arr[left] <= arr[right]){
+      temp.push_back(arr[left]);
+      left++;
+    }
+    else{
+      temp.push_back(arr[right]);
+      right++;
+    }
+  }
+  while(left <= mid){
+        temp.push_back(arr[left]);
+        left++;
+    }
+
+  while(right <= high){
+      temp.push_back(arr[right]);
+      right++;
+  }
+
+  //to print the sorted array
+  for(int i = low; i <= high; i++){
+    arr[i] = temp[i - low];
+  }
+}
+
+void merge_sort(int arr[], int low, int high){
+  if(low >= high) return; //base case
+  int mid = (high + low) / 2;
+
+  //recursive calls
+  merge_sort(arr, low, mid);
+  merge_sort(arr, mid + 1, high);
+
+  //calling merge function to combine final array
+  merge(arr, low, mid, high);
+}
+
 int main(){
   int n;
   cin >> n;
@@ -51,7 +95,8 @@ int main(){
   for(int i = 0; i < n; i++) cin >> arr[i];
   // selection_sort(n , arr);
   // bubble_sort(n , arr);
-  insertion_sort(n , arr);
+  // insertion_sort(n , arr);
+  merge_sort(arr, 0, n-1);
   for(int i = 0; i < n; i++) cout << arr[i] << " ";
   return 0;
 }
