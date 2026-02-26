70232362f5eed46684fb329e0060aaaf986fa158
Pratham Gupta <prathamgupta0405@gmail.com>
2026-02-27 03:08:21 +0530

    feat: add array1.cpp with a function to find the largest element and output example

---
+++ Arrays/array1.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+void largest_element(){}
+int main(){
+  int n = 5;
+  int arr[5] = {1,2,3,4,5};
+  for(int i = 0; i < n; i++){
+    cout << arr[4];
+  }
+  return 0;
+}
