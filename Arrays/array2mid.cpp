2d804033be0173442cfa8f6f8f09637fd227c3e4
Pratham Gupta <prathamgupta0405@gmail.com>
2026-03-26 15:12:25 +0530

    implementing largest sequence in an array

--- Arrays/array2mid.cpp
+++ Arrays/array2mid.cpp
@@ -320,21 +320,41 @@ int longestsequence(int n, int arr[]){
    // return longest;
 
    //better approach
+   // if(n == 0) return 0;
+   // sort(arr, arr+n);
+   // int lastSmaller = INT_MIN;
+   // int cnt = 0;
+   // int longest = 1;
+   // for(int i = 0; i < n; i++){
+   //    if(arr[i] - 1 == lastSmaller){
+   //       cnt += 1;
+   //       lastSmaller = arr[i];
+   //    }
+   //    else if(lastSmaller != arr[i]){
+   //       cnt = 1;
+   //       lastSmaller = arr[i];
+   //    }
+   //    longest = max(longest, cnt);
+   // }
+   // return longest;
+
+   //optimal soution
    if(n == 0) return 0;
-   sort(arr, arr+n);
-   int lastSmaller = INT_MIN;
-   int cnt = 0;
    int longest = 1;
+   unordered_set<int> st;
    for(int i = 0; i < n; i++){
-      if(arr[i] - 1 == lastSmaller){
-         cnt += 1;
-         lastSmaller = arr[i];
+      st.insert(arr[i]);
+   }
+   for(auto it : st){
+      if(st.find(it - 1) == st.end()){
+         int cnt = 1;
+         int x = it;
+         while(st.find(x+1) != st.end()){
+            x += 1;
+            cnt += 1;
+         }
+         longest = max(longest, cnt);
       }
-      else if(lastSmaller != arr[i]){
-         cnt = 1;
-         lastSmaller = arr[i];
-      }
-      longest = max(longest, cnt);
    }
    return longest;
 }
