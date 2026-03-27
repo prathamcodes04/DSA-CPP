09f01928bd9d1cef1ec2d41a0155b42b92c41d46
Pratham Gupta <prathamgupta0405@gmail.com>
2026-03-27 23:13:17 +0530

    implementing optimal solution for printing the matrix in spiral manner

--- Arrays/array2mid.cpp
+++ Arrays/array2mid.cpp
@@ -481,6 +481,40 @@ void rotatematrix(vector<vector<int>>& matrix){
    }
 }
 
+void matrixspiralmanner(vector<vector<int>>& matrix){
+   int n = matrix.size();
+   int m = matrix[0].size();
+   int left = 0, right = m - 1;
+   int top = 0, bottom = n - 1;
+   vector<int> ans;
+   while(left <= right && top <= bottom){
+      //left -> right
+      for(int i = left; i <= right; i++){
+         ans.push_back(matrix[top][i]);
+      }
+      top++;
+      for(int i = top; i <= bottom; i++){
+         ans.push_back(matrix[i][right]);
+      }
+      right--;
+      if(top <= bottom){
+         for(int i = right; i >= left; i--){
+            ans.push_back(matrix[bottom][i]);
+         }
+         bottom--;
+      }
+      if(left <= right){
+         for(int i = bottom; i >= top; i--){
+            ans.push_back(matrix[i][left]);
+         }
+         left++;
+      }
+   }
+   for(auto it : ans){
+      cout << it << " ";
+   }
+}
+
 int main(){
    // int n = 9;
    vector<vector<int>> matrix = {
@@ -488,13 +522,13 @@ int main(){
    {5,6,7,8},
    {9,10,11,12},
    {13,14,15,16}};
-   rotatematrix(matrix);
-   for(auto row : matrix){
-      for(auto val : row){
-         cout << val << " ";
-      }
-      cout << endl;
-   }
+   matrixspiralmanner(matrix);
+   // for(auto row : matrix){
+   //    for(auto val : row){
+   //       cout << val << " ";
+   //    }
+   //    cout << endl;
+   // }
    // cout << longestsequence(n, arr);
    // leaderinanarray(n, arr);
    // nextpermutations(n, arr);
