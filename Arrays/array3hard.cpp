5ff59cd8cb44f0fd8b311e376926b963259c8efc
Pratham Gupta <prathamgupta0405@gmail.com>
2026-04-12 03:22:26 +0530

    Implement optimal two-pointer approach for three sum problem and update output format

--- Arrays/array3hard.cpp
+++ Arrays/array3hard.cpp
@@ -108,20 +108,48 @@ vector<vector<int>> threesum(int arr[], int n){
     // return ans;
 
     //better approach
-    set<vector<int>> st;
+    // set<vector<int>> st;
+    // for(int i = 0; i < n; i++){
+    //     set<int> hashset;
+    //     for(int j = i + 1; j < n; j++){
+    //         int third = -(arr[i] + arr[j]);
+    //         if(hashset.find(third) != hashset.end()){
+    //             vector<int> temp = {arr[i], arr[j], third};
+    //             sort(temp.begin(), temp.end());
+    //             st.insert(temp);
+    //         }
+    //         hashset.insert(arr[j]);
+    //     }
+    // }
+    // vector<vector<int>> ans(st.begin(), st.end());
+    // return ans;
+
+    //optimal approach - two pointer
+    vector<vector<int>> ans;
+    sort(arr, arr + n);
     for(int i = 0; i < n; i++){
-        set<int> hashset;
-        for(int j = i + 1; j < n; j++){
-            int third = -(arr[i] + arr[j]);
-            if(hashset.find(third) != hashset.end()){
-                vector<int> temp = {arr[i], arr[j], third};
-                sort(temp.begin(), temp.end());
-                st.insert(temp);
+        //skip duplicates for first element nnnn
+        if(i > 0 && arr[i] == arr[i-1]) continue;
+        int left = i + 1;
+        int right = n - 1;
+        while(left < right){
+            int sum = arr[i] + arr[left] + arr[right];
+            if(sum < 0){
+                left++;
             }
-            hashset.insert(arr[j]);
+            else if(sum > 0){
+                right--;
+            }
+            else{
+                ans.push_back({arr[i], arr[left], arr[right]});
+                left++;
+                right--;
+                //skippig duplicates
+                while(left < right && arr[left] == arr[left - 1]) left++;
+                while(left < right && arr[right] == arr[right + 1]) right--;
+            }
         }
     }
-    vector<vector<int>> ans(st.begin(), st.end());
     return ans;
 }
 
