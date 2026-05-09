7c83dbe0e1690becc2b594d4a325bb83acac44b9
Pratham Gupta <prathamgupta0405@gmail.com>
2026-05-09 15:56:07 +0530

    add optimal approach for merging intervals

--- Arrays/1corearrayquestions.cpp
+++ Arrays/1corearrayquestions.cpp
@@ -242,6 +242,47 @@ int search(vector<int>& nums, int target) {
     return -1;
 }
 
+//merge intervals
+vector<vector<int>> merge(vector<vector<int>>& intervals) {
+    //overlap when current_start <= previous_end
+    //if overlap exists merge them
+
+    //bruteforce approach 
+    // sort(intervals.begin(), intervals.end());
+    // vector<vector<int>> ans;
+    // for(int i = 0; i < intervals.size(); i++){
+    //     int start = intervals[i][0];
+    //     int end = intervals[i][1];
+    //     if(!ans.empty() && end <= ans.back()[1]){
+    //         continue;
+    //     }
+    //     for(int j = i + 1; j < intervals.size(); j++){
+    //         if(intervals[j][0] <= end){
+    //             end = max(end, intervals[j][1]);
+    //         }
+    //         else{
+    //             break;
+    //         }
+    //     }
+    //     ans.push_back({start,end});
+    // }
+    // return ans;
+
+    //better approach
+    sort(intervals.begin(), intervals.end());
+    vector<vector<int>> ans;
+    for(int i = 0; i < intervals.size(); i++){
+        if(ans.empty() || intervals[i][0] > ans.back()[1]){
+            ans.push_back(intervals[i]);
+        }
+        else{
+            //take larger ending point
+            ans.back()[1] = max(ans.back()[1], intervals[i][1]);
+        }
+    }
+    return ans;
+}
+
 int main(){
     int a = 1;
     int b = 2;
