795907d59d3040a18a99bb5f8a0ee93a3a8ff2f6
Pratham Gupta <prathamgupta0405@gmail.com>
2026-06-29 16:43:01 +0530

    implementing second largest element in an array

--- Arrays/top30array.cpp
+++ Arrays/top30array.cpp
@@ -17,8 +17,44 @@ int largestElement(vector<int>& arr){
     // return largest;
 }
 
+//second largest element in an array
+int secondLargestElement(vector<int>& arr){
+    //brute force - sorting
+    // sort(arr.begin(), arr.end());
+    // return arr[arr.size() - 2];
+
+    //better solution - first find largest then find second largest
+    // int largest = INT_MIN;
+    // int slargest = INT_MIN;
+    // for(int i = 0; i < arr.size(); i++){
+    //     if(arr[i] > largest){
+    //         largest = arr[i];
+    //     }
+    // }
+    // for(int i = 0; i < arr.size(); i++){
+    //     if(arr[i] > slargest && arr[i] != largest){
+    //         slargest = arr[i];
+    //     }
+    // }
+    // return slargest;
+
+    //optimal solution using else if, one pass 
+    int largest = INT_MIN;
+    int slargest = INT_MIN;
+    for(int i = 0; i < arr.size(); i++){
+        if(arr[i] > largest){
+            slargest = largest;
+            largest = arr[i];
+        }
+        else if(arr[i] < largest && arr[i] > slargest){
+            slargest = arr[i];
+        }
+    }
+    return slargest;
+}
+
 int main(){
     vector<int> arr = {2,5,1,3,0};
-    cout << largestElement(arr);
+    cout << secondLargestElement(arr);
     return 0;
 }
